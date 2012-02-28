d13ae67e3e63a58c3624823a4e74417843207fd8
Maarten van der Velden <m.m.vandervelden@uva.nl>
2012-02-28 15:55:22 +0100

    batch__densesift works, but perhaps selecting first is better for disk usage

--- batch_densesift.py
+++ batch_densesift.py
@@ -4,12 +4,11 @@
 batch_densesift.py
 
 Created by Maarten van der Velden on 2012-02-21.
-Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+Copyright (c) 2012 UvA. All rights reserved.
 """
 
 import argparse, os
 from subprocess import call
-
 
 def runBatch(args):
     path = args.data
@@ -17,16 +16,19 @@ def runBatch(args):
     if not 'descriptors' in files:
         os.mkdir(path+'/descriptors')
     outputbase = path+"descriptors/"+args.detector + "_" + args.descriptor + "_"
-    cdeopts = ["--detector", args.detector, "--descriptor", args.descriptor, "--output"]
-    outputext = '.cde'
-    
+    if args.binary:
+        cdeopts = ["--detector", args.detector, "--descriptor", args.descriptor, "--outputFormat", "binary", "--output"]
+        outputext = '.dbin'
+    else:
+        cdeopts = ["--detector", args.detector, "--descriptor", args.descriptor, "--output"]        
+        outputext = '.dko'
     for f in files:
         if args.verbose:
             print "generating descriptors for %s"% f
         
         if f[-4:] == ".jpg":
             o = outputbase+f[:-4]+outputext
-            run_args= ['./colorDescriptor', path+f] + cdeopts + [o]
+            run_args= ['colorDescriptor', path+f] + cdeopts + [o]
             #print run_args
             #print type(run_args)
             res = call(run_args)
@@ -39,11 +41,13 @@ if __name__ == '__main__':
     parser.add_argument('-v', dest='verbose', action='store_true')
     parser.add_argument('-t', '--detector',default='densesampling')
     parser.add_argument('-s', '--descriptor', default='sift')
+    parser.add_argument('-b', '--binary', action='store_true')
     args = parser.parse_args()
     
     if args.verbose:
         print 'data: ', args.data
         print 'detector: ', args.detector
         print 'descriptor: ', args.descriptor
+        print 'binary: ', args.binary
     runBatch(args)
     
