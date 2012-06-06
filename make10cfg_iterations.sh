77e65a101ca2277eefcbbb32a54854ac6ca13746
Maarten van der Velden <m.m.vandervelden@uva.nl>
2012-06-06 13:41:10 +0200

    clean master for merge with fast_segmentation

--- make10cfg_iterations.sh
+++
@@ -1,8 +0,0 @@
-echo $1
-for it in `seq 2 10`; do
-	s='s/_1/_'$it'/'
-	o=$1'_'$it'.cfg'
-	echo $o
-	cat $1_1.cfg | sed $s > $o
-	cat $o | grep $it
-done
--- run_5_paralel.sh
+++
@@ -1,4 +0,0 @@
-for it in `seq 1 5`; do
-	echo $it
-	qsub -v SCRIPT=bcal$it.cfg run_1.job
-done
