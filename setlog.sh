93eca5c4bfd81ea80b185f14de4bf98921c26ce1
Maarten van der Velden <m.m.vandervelden@uva.nl>
2012-06-26 10:42:43 +0200

    made script to handle making temporary new log configs

---
+++ setlog.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# $1 = destination logfile
+# $2 = write mode (w or a)
+# add .cfg and replace slashes to be escaped ('s/ \/ / \\ \/ /')
+NLOG=$(echo $1 | sed 's/\//\\\//')
+NLOGCFG=$1.cfg
+less blank.log.cfg | sed "s/'blank.log','a'/'$NLOG','$2'/" > $NLOGCFG
