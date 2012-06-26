4d202f2d408a7b5fd93a9aded318b1dba21bfadc
Maarten van der Velden <m.m.vandervelden@uva.nl>
2012-06-26 11:24:31 +0200

    bug in setlogs.sh: escape all slashes in path (/g option of sed)

--- setlog.sh
+++ setlog.sh
@@ -2,6 +2,6 @@
 # $1 = destination logfile
 # $2 = write mode (w or a)
 # add .cfg and replace slashes to be escaped ('s/ \/ / \\ \/ /')
-NLOG=$(echo $1 | sed 's/\//\\\//')
+NLOG=$(echo $1 | sed 's/\//\\\//g')
 NLOGCFG=$1.cfg
 less blank.log.cfg | sed "s/'blank.log','a'/'$NLOG','$2'/" > $NLOGCFG
