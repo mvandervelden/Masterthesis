83ec06f49e480b5c37b3d99acd050efd06bf599a
Maarten van der Velden <m.m.vandervelden@uva.nl>
2012-07-24 16:12:15 +0200

    debug best cluster, should be ndarray

--- voc_fgbg_det_exemplar_test.py
+++ voc_fgbg_det_exemplar_test.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
             best_cluster_idx = cluster_hypotheses(overlap, indexes, DETECTIONopts['theta_m'])
             # merge the biggest cluster of hypotheses into a detection, and append it
             log.debug(' Best cluster size: %d',best_cluster_idx.shape[0])
-            detections.append(merge_cluster(hypotheses[best_cluster_idxs], im_ids[i]))
+            detections.append(merge_cluster(hypotheses[best_cluster_idx], im_ids[i]))
             # Select hypotheses to remove based on the cluster and the removal threshold theta_p
             hypotheses, overlap, indexes = remove_clusters(best_cluster_idx, detection[3:], hypotheses,overlap, indexes, DETECTIONopts['theta_p'])
             
