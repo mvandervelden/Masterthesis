c9113a2d198cd19bb8ffe23621774e11d735da3c
Maarten van der Velden <m.m.vandervelden@uva.nl>
2012-08-20 15:15:21 +0200

    preparing a large test

--- get_memuse.sh
+++ get_memuse.sh
@@ -1,3 +1,4 @@
-
-cat $1 | awk -F "[^0-9]*" '{ if (min==""){min=max=$8}; if ($8>max){max=$8}; if ($8<min){min=$8}; total+=$8;count+=1} END {print "Memory use: min: ", min, "max: ", max, "av: ", total/count}'
-cat $1 | awk -F "[^0-9]*" '{ if (min==""){min=max=$9}; if ($9>max){max=$9}; if ($9<min){min=$9}; total+=$9;count+=1} END {print "Swap   use: min: ", min, "max: ", max, "av: ", total/count}'
+#!/bin/bash
+echo $1
+cat $1 | awk -F "[^0-9]*" '{ if (min==""){min=max=$8}; if ($8>max){max=$8}; if ($8<min){min=$8}; total+=$8;count+=1} END {printf "Memory use: min: %8dkb max: %8dkb, av:%8dkb\n", min, max, total/count}'
+cat $1 | awk -F "[^0-9]*" '{ if (min==""){min=max=$9}; if ($9>max){max=$9}; if ($9<min){min=$9}; total+=$9;count+=1} END {printf "Swap   use: min: %8dkb max: %8dkb, av:%8dkb\n", min, max, total/count}'
--- voc_3step.sh
+++ voc_3step.sh
@@ -4,22 +4,23 @@ echo "Running training"
 CFGFILE=$1
 TMPFOLDER=$2
 
-mkdir "$TMPFOLDER"
-
-python voc_det_exemplar_train.py $CFGFILE $TMPFOLDER
+# mkdir "$TMPFOLDER"
+# 
+# python voc_det_exemplar_train.py $CFGFILE $TMPFOLDER
 NO_BATCHES=`cat $TMPFOLDER/testinfo.txt | sed -n '1p'`
 NO_CLASSES=`cat $TMPFOLDER/testinfo.txt | sed -n '2p'`
 CLASSES=(`cat $TMPFOLDER/testinfo.txt | tail -n 1`)
-THREADS=4
+TESTTHREADS=4
+DETECTTHREADS=8
 echo "No of batches: $NO_BATCHES"
 echo "No of classes: $NO_CLASSES"
 echo "Classes: ${CLASSES[@]}"
 
 # all classes, and how many processes there should be in each iteration
-NO_FULL_ITS=`echo $(($NO_CLASSES/$THREADS))`
+NO_FULL_ITS=`echo $(($NO_CLASSES/$TESTTHREADS))`
 if [ $NO_FULL_ITS -gt 0 ]; then
     FULL_ITS=`seq 1 $NO_FULL_ITS`
-    IT_SIZES=`for it in $FULL_ITS; do echo $THREADS; done; echo $(($NO_CLASSES%$THREADS))`
+    IT_SIZES=`for it in $FULL_ITS; do echo $TESTTHREADS; done; echo $(($NO_CLASSES%$TESTTHREADS))`
 else
     IT_SIZES=$NO_CLASSES
 fi
@@ -43,12 +44,27 @@ done
 # perform detection (clustering on all images) per image
 for B in `seq 1 $NO_BATCHES`; do
     IMIDS=(`cat $TMPFOLDER/batch_$B.pkl.txt`)
-    echo "Running batch $B on detection: img: ${IMIDS[@]}"
+    NO_IMIDS=${#IMIDS[@]}
+    NO_FULL_ITS=`echo $(($NO_IMIDS/$DETECTTHREADS))`
+    if [ $NO_FULL_ITS -gt 0 ]; then
+        FULL_ITS=`seq 1 $NO_FULL_ITS`
+        IT_SIZES=`for it in $FULL_ITS; do echo $DETECTTHREADS; done; echo $$(($NO_IMIDS%$DETECTTHREADS))`
+    else
+        IT_SIZES=$NO_IMIDS
+    fi
     for CLS in ${CLASSES[@]}; do
         echo "Running detection on class $CLS"
-        for ID in ${IMIDS[@]}; do
-            echo "Running detection on image $ID"
-            python voc_det_exemplar_detect.py $CFGFILE $TMPFOLDER $B $CLS $ID
+        START_IMID=0
+        for SZ in $IT_SIZES; do
+            echo "Running batch $B on detection. $SZ images simultaneously"
+            STOP_IMID=$(($START_IMID+$SZ-1))
+            for I in `seq $START_IMID $STOP_IMID`; do
+                IMID=${IMIDS[$I]}
+                echo "Running detection on image no $I ($IMID)"
+                python voc_det_exemplar_detect.py $CFGFILE $TMPFOLDER $B $CLS $IMID&
+            done
+            wait
+            START_IMID=$(($START_IMID+$SZ))
         done
     done
 done
