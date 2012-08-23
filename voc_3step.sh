916dea9c5b80767b0dbf9391942405e75924cf31
Maarten van der Velden <m.m.vandervelden@uva.nl>
2012-08-23 12:01:09 +0200

    Edited voc_3step.sh for multiple threads on detection

--- voc_3step.sh
+++ voc_3step.sh
@@ -7,6 +7,7 @@ TMPFOLDER=$2
 # mkdir "$TMPFOLDER"
 # 
 # python voc_det_exemplar_train.py $CFGFILE $TMPFOLDER
+python voc_det_exemplar_mktest.py $CFGFILE $TMPFOLDER
 NO_BATCHES=`cat $TMPFOLDER/testinfo.txt | sed -n '1p'`
 NO_CLASSES=`cat $TMPFOLDER/testinfo.txt | sed -n '2p'`
 CLASSES=(`cat $TMPFOLDER/testinfo.txt | tail -n 1`)
@@ -17,29 +18,29 @@ echo "No of classes: $NO_CLASSES"
 echo "Classes: ${CLASSES[@]}"
 
 # all classes, and how many processes there should be in each iteration
-NO_FULL_ITS=`echo $(($NO_CLASSES/$TESTTHREADS))`
-if [ $NO_FULL_ITS -gt 0 ]; then
-    FULL_ITS=`seq 1 $NO_FULL_ITS`
-    IT_SIZES=`for it in $FULL_ITS; do echo $TESTTHREADS; done; echo $(($NO_CLASSES%$TESTTHREADS))`
-else
-    IT_SIZES=$NO_CLASSES
-fi
-
-for B in `seq 1 $NO_BATCHES`; do
-    echo "Running batch $B"
-    START_CLS=0
-    for SZ in $IT_SIZES; do
-        echo "Running iteration of $SZ processes"
-        STOP_CLS=$(($START_CLS+$SZ-1))
-        for P in `seq $START_CLS $STOP_CLS`; do
-            CLS=${CLASSES[$P]}
-            echo "Running test on class no $P ($CLS)"
-            python voc_det_exemplar_test.py $CFGFILE $TMPFOLDER $B $CLS&
-        done
-        wait
-        START_CLS=$(($START_CLS+$SZ))
-    done
-done
+# NO_FULL_ITS=`echo $(($NO_CLASSES/$TESTTHREADS))`
+# if [ $NO_FULL_ITS -gt 0 ]; then
+#     FULL_ITS=`seq 1 $NO_FULL_ITS`
+#     IT_SIZES=`for it in $FULL_ITS; do echo $TESTTHREADS; done; echo $(($NO_CLASSES%$TESTTHREADS))`
+# else
+#     IT_SIZES=$NO_CLASSES
+# fi
+# 
+# for B in `seq 1 $NO_BATCHES`; do
+#     echo "Running batch $B"
+#     START_CLS=0
+#     for SZ in $IT_SIZES; do
+#         echo "Running iteration of $SZ processes"
+#         STOP_CLS=$(($START_CLS+$SZ-1))
+#         for P in `seq $START_CLS $STOP_CLS`; do
+#             CLS=${CLASSES[$P]}
+#             echo "Running test on class no $P ($CLS)"
+#             python voc_det_exemplar_test.py $CFGFILE $TMPFOLDER $B $CLS&
+#         done
+#         wait
+#         START_CLS=$(($START_CLS+$SZ))
+#     done
+# done
 
 # perform detection (clustering on all images) per image
 for B in `seq 1 $NO_BATCHES`; do
@@ -48,7 +49,7 @@ for B in `seq 1 $NO_BATCHES`; do
     NO_FULL_ITS=`echo $(($NO_IMIDS/$DETECTTHREADS))`
     if [ $NO_FULL_ITS -gt 0 ]; then
         FULL_ITS=`seq 1 $NO_FULL_ITS`
-        IT_SIZES=`for it in $FULL_ITS; do echo $DETECTTHREADS; done; echo $$(($NO_IMIDS%$DETECTTHREADS))`
+        IT_SIZES=`for it in $FULL_ITS; do echo $DETECTTHREADS; done; echo $(($NO_IMIDS%$DETECTTHREADS))`
     else
         IT_SIZES=$NO_IMIDS
     fi
