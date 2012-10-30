e6378730ddb28e367f71d0fa51cb1577c3645ca2
Maarten van der Velden <m.m.vandervelden@uva.nl>
2012-10-30 11:45:34 +0100

    copy cfg to resfolder to avoid changes while testing, new das test

--- detection_script.sh
+++ detection_script.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# mkdir /local/vdvelden
 
 CFGFILE=$1
 echo "Running training"
 TMPFOLDER=`cat $CFGFILE | awk '$1 ~ /tmp_dir/ { print $3 }'`
+RESFOLDER=`cat $CFGFILE | awk '$1 ~ /res_dir/ { print $3 }'`
 
-python train_detection.py $CFGFILE
-echo "Making batches"
-python make_detection_batches.py $CFGFILE
+# mkdir $RESFOLDER
+# cp $CFGFILE $RESFOLDER
+CFGFILE=`echo "${RESFOLDER}/${CFGFILE}"`
+echo "CFGFILE COPIED TO: $CFGFILE"
+
+# python train_detection.py $CFGFILE
+# echo "Making batches"
+# python make_detection_batches.py $CFGFILE
 
 echo "Reading cfg $CFGFILE"
 NNTHREADS=`cat $CFGFILE | awk '$1 ~ /nn_threads/ { print $3 }'`
@@ -25,31 +32,31 @@ echo "Classes: ${CLASSES[@]}"
 echo ""
 echo "Running NN for all batches and classes"
 
-NO_FULL_ITS=`echo $(($NO_CLASSES/$NNTHREADS))`
-if [ $NO_FULL_ITS -gt 0 ]; then
-    FULL_ITS=`seq 1 $NO_FULL_ITS`
-    IT_SIZES=`for it in $FULL_ITS; do echo $NNTHREADS; done; REM=$(($NO_CLASSES%$NNTHREADS)); if [ $REM != 0 ]; then echo $REM; fi`
-else
-    IT_SIZES=$NO_CLASSES
-fi
-echo "Iterations: $NO_FULL_ITS"
-echo "FULL_ITS: $FULL_ITS"
-echo "It_sizes: $IT_SIZES"
-for B in `seq 1 $NO_BATCHES`; do
-    echo "Running batch $B"
-    START_CLS=0
-    for SZ in $IT_SIZES; do
-        echo "Running iteration of $SZ processes"
-        STOP_CLS=$(($START_CLS+$SZ-1))
-        for P in `seq $START_CLS $STOP_CLS`; do
-            CLS=${CLASSES[$P]}
-            echo "Running NN on class no $P ($CLS)"
-            python get_detection_distances.py $CFGFILE $B $CLS&
-        done
-        wait
-        START_CLS=$(($START_CLS+$SZ))
-    done
-done
+# NO_FULL_ITS=`echo $(($NO_CLASSES/$NNTHREADS))`
+# if [ $NO_FULL_ITS -gt 0 ]; then
+#     FULL_ITS=`seq 1 $NO_FULL_ITS`
+#     IT_SIZES=`for it in $FULL_ITS; do echo $NNTHREADS; done; REM=$(($NO_CLASSES%$NNTHREADS)); if [ $REM != 0 ]; then echo $REM; fi`
+# else
+#     IT_SIZES=$NO_CLASSES
+# fi
+# echo "Iterations: $NO_FULL_ITS"
+# echo "FULL_ITS: $FULL_ITS"
+# echo "It_sizes: $IT_SIZES"
+# for B in `seq 1 $NO_BATCHES`; do
+#     echo "Running batch $B"
+#     START_CLS=0
+#     for SZ in $IT_SIZES; do
+#         echo "Running iteration of $SZ processes"
+#         STOP_CLS=$(($START_CLS+$SZ-1))
+#         for P in `seq $START_CLS $STOP_CLS`; do
+#             CLS=${CLASSES[$P]}
+#             echo "Running NN on class no $P ($CLS)"
+#             python get_detection_distances.py $CFGFILE $B $CLS&
+#         done
+#         wait
+#         START_CLS=$(($START_CLS+$SZ))
+#     done
+# done
 
 echo "Running detection"
 # perform detection (clustering on all images) per image
@@ -89,10 +96,9 @@ for CLS in ${CLASSES[@]}; do
     python rank_detections.py $CFGFILE $CLS
 done
 
-echo "Tarballing results and tmpfiles"
-tar -czvf ${CFGFILE}.tmp.tgz --exclude=*.dbin --exclude=*.dtxt --exclude=*.data --exclude=*.index $TMPFOLDER
-RESFOLDER=`cat $CFGFILE | awk '$1 ~ /res_dir/ { print $3 }'`
-tar -czvf ${CFGFILE}.res.tgz $RESFOLDER
+# echo "Tarballing results and tmpfiles"
+# tar -czvf ${CFGFILE}.tmp.tgz --exclude=*.dbin --exclude=*.dtxt --exclude=*.data --exclude=*.index $TMPFOLDER
+# tar -czvf ${CFGFILE}.res.tgz $RESFOLDER
 
 echo "FINISHED ALL"
 
